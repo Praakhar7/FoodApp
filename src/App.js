69d54afe90cda3a04b1ae3a482ce79feec73a29d
Praakhar7 <120710232+Praakhar7@users.noreply.github.com>
2023-02-15 12:56:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 boostrap
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
